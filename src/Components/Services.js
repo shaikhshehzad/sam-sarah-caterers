0ff2101582aca66957d90dcb00e1c037fc85ed6c
shaikhshehzad <shehzadali3113@gmail.com>
2022-09-20 00:40:10 +0530

    Home Link Issue

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 export default class Services extends Component {
   render() {
     return (
-      <div>Services</div>
+      <div> <div>
+      Sam-Sarah Caterers Coming Live soon.........</div>
+      <div>
+     Reach out to us on below contacts</div>
+    <div>
+    email:- samsarahcaterers@gmail.com</div>
+     <div>
+    Contact Information :-  +91 9822447649
+    </div></div>
     )
   }
 }
