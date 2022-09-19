1f6649edec36ce58e51c5f318bbc6a3f547dd755
shaikhshehzad <shehzadali3113@gmail.com>
2022-09-20 00:37:16 +0530

    Home Link Issue

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Carouselgallery from './HomeComponents/Carouselgallery'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+     <>
+     <Carouselgallery/>
+     </>
     )
   }
 }
---
+++ src/Components/HomeComponents/Carouselgallery.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+export default class Carouselgallery extends Component {
+  render() {
+    return (
+      <div>
+         <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://source.unsplash.com/1000x500/?nachos"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://source.unsplash.com/1000x500/?burger"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://source.unsplash.com/1000x500/?pizza"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+
+      </div>
+    )
+  }
+}
